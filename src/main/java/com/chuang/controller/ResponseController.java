de0692942e5e80e975c4511efbc15793c4923eab
huang <chanlyhung@icloud.com>
2020-04-17 06:13:02 +0900

    update testModelAndView

--- src/main/java/com/chuang/controller/ResponseController.java
+++ src/main/java/com/chuang/controller/ResponseController.java
@@ -4,6 +4,12 @@ import com.chuang.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @author chuang
@@ -29,4 +35,45 @@ public class ResponseController {
         return "success";
     }
 
+    @RequestMapping("/testVoid")
+    public void testVoid(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        System.out.println("testVoid execute");
+
+        // 请求转发
+//        request.getRequestDispatcher("/WEB-INF/pages/success.jsp").forward(request,response);
+
+        // 重定向（新的请求路径）
+//        response.sendRedirect(request.getContextPath()+"/index.jsp");
+
+        // 设置中文乱码问题
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
+
+
+       // 直接相应
+        response.getWriter().println("你好!!!");
+
+        return;
+    }
+
+
+    @RequestMapping("/testModelAndView")
+    public ModelAndView testModelAndView(){
+
+        ModelAndView modelAndView = new ModelAndView();
+        System.out.println("testModelAndView execute");
+
+        User user = new User();
+        user.setName("井上");
+        user.setPassword("123456");
+        user.setAge(23);
+
+        modelAndView.addObject("user", user);
+
+        modelAndView.setViewName("success");
+
+        return modelAndView;
+    }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,8 +1,11 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2>重定向成功</h2>
 
-<a href="res/testResponse">Response Controlle</a>
+<%--<a href="res/testResponse">Response Controller</a><br>--%>
+
+<%--<a href="res/testVoid">VOID Test</a><br>--%>
+
 </body>
 </html>
